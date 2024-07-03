7dd21432a2fc098969544106ef63c9e68a673d40
itpark1018 <itpark1018@gmail.com>
2024-07-03 23:15:49 +0900

    (common-setting)
    RedisConfiguration, SecurityConfiguratio 추가
    CustomException 추가

---
+++ src/main/java/com/fantion/backend/configuration/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.fantion.backend.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    return http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(requests -> requests.requestMatchers("/payment/save", "payment/success/**").permitAll())
+        .build();
+  }
+}
