d5b1865ee1cbcbd59d6e4db40afbac38649bd6f9
SeungMinKim <alphagoldksm90@naver.com>
2024-07-03 10:43:52 +0900

    starting point

--- src/main/java/com/fantion/backend/auction/controller/AuctionController.java
+++ src/main/java/com/fantion/backend/auction/controller/AuctionController.java
@@ -2,7 +2,6 @@ package com.fantion.backend.auction.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @RequestMapping("/auction")
 public class AuctionController {
