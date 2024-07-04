c71c74df8c168d692dd62f78989d6edee573dca4
SeungMinKim <alphagoldksm90@naver.com>
2024-07-04 10:12:08 +0900

    restart

--- src/main/java/com/fantion/backend/payment/exception/HoldingIllegalStateException.java
+++
@@ -1,9 +0,0 @@
-package com.fantion.backend.payment.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.CONFLICT)
-public class HoldingIllegalStateException extends RuntimeException {
-
-}
--- src/main/java/com/fantion/backend/payment/exception/InvalidRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.fantion.backend.payment.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class InvalidRequestException extends RuntimeException{
-
-}
