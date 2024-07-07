b2d9daaf2aa884e7c3d7573062ca67709972bec0
SeungMinKim <alphagoldksm90@naver.com>
2024-07-07 21:59:44 +0900

    ê²gradle Essential Used Pharsing

--- src/main/java/com/fantion/backend/payment/payment_cancel/repository/PaymentWebhookRepository.java
+++ src/main/java/com/fantion/backend/payment/payment_cancel/repository/PaymentWebhookRepository.java
@@ -2,7 +2,7 @@ package com.fantion.backend.payment.payment_cancel.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import sigma.Spring_backend.payment.entity.PaymentWebhook;
+import com.fantion.backend.payment.payment_cancel.entity.PaymentWebhook;
 
 @Repository
 public interface PaymentWebhookRepository extends JpaRepository<PaymentWebhook, Long> {
