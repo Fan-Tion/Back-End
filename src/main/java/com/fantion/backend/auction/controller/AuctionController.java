2719664a425753e2db14dd644c791dbde839d0b3
YSG <dbtjrrbs12@naver.com>
2024-07-07 22:15:09 +0900

    Feat : 입찰가 갱신 및 경매 상세보기 추가

--- src/main/java/com/fantion/backend/auction/controller/AuctionController.java
+++ src/main/java/com/fantion/backend/auction/controller/AuctionController.java
@@ -1,10 +1,10 @@
 package com.fantion.backend.auction.controller;
 
+import com.fantion.backend.auction.dto.AuctionDto;
 import com.fantion.backend.auction.dto.BidDto;
 import com.fantion.backend.auction.service.AuctionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
@@ -13,10 +13,16 @@ import org.springframework.web.bind.annotation.*;
 public class AuctionController {
 
     private final AuctionService auctionService;
+
     @PutMapping("/bid")
     private BidDto.Response createBid(@RequestBody BidDto.Request request){
         log.info("[Controller] createBid");
         return  auctionService.createBid(request);
+    }
+
+    @GetMapping("/view/{auctionId}")
+    private AuctionDto.Response findAuction(@PathVariable(name = "auctionId") Long auctionId){
+        return auctionService.findAuction(auctionId);
     }
 
 }
