60c81714426bdab13c314f79808b9cff9209e066
YSG <dbtjrrbs12@naver.com>
2024-07-16 03:34:36 +0900

    Refactor : Conflict 수정

--- src/main/java/com/fantion/backend/exception/CustomExceptionHandler.java
+++ src/main/java/com/fantion/backend/exception/CustomExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.fantion.backend.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -18,6 +20,18 @@ public class CustomExceptionHandler {
 
     return new ResponseEntity<>(errorResponse, e.getHttpStatus());
   }
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException e) {
+    ErrorResponse errorResponse = ErrorResponse.builder()
+            .status(HttpStatus.BAD_REQUEST)
+            .errorCode("VALIDATION_ERROR") // 일반적인 에러 코드 사용
+            .message("Validation failed: " + e.getBindingResult().getFieldError()
+                    .getDefaultMessage()) // 첫 번째 오류 메시지 사용
+            .build();
+
+    return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+  }
+
   @ExceptionHandler(FantionAbstractException.class)
   protected ResponseEntity<FantionErrorResponse> fantionCustomException(FantionAbstractException e) {
 
