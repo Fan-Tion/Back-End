2f9c164031792245ecac5888ec328f16a74bde4e
itpark1018 <117416583+itpark1018@users.noreply.github.com>
2024-07-21 19:59:21 +0900

    Update SecurityConfiguration.java

--- src/main/java/com/fantion/backend/configuration/SecurityConfiguration.java
+++ src/main/java/com/fantion/backend/configuration/SecurityConfiguration.java
@@ -1,15 +1,20 @@
 package com.fantion.backend.configuration;
 
 import com.fantion.backend.member.jwt.JwtTokenFilter;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -31,5 +36,17 @@ public class SecurityConfiguration {
             SessionCreationPolicy.STATELESS))
         .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.addAllowedOriginPattern("*"); // 모든 출처를 허용합니다.
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드를 설정합니다.
+    configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더를 허용합니다.
+    configuration.setAllowCredentials(true); // 쿠키 인증을 허용합니다.
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
