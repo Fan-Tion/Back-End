ca59185c297c269bdfdc63512180ae3277c3fdd5
itpark1018 <itpark1018@gmail.com>
2024-07-30 18:59:52 +0900

    (refactor:toss-cancel)

--- src/main/java/com/fantion/backend/payment/dto/CancelDto.java
+++ src/main/java/com/fantion/backend/payment/dto/CancelDto.java
@@ -1,5 +1,9 @@
 package com.fantion.backend.payment.dto;
 
+import com.fantion.backend.type.BalanceType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,7 +15,13 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
+@Schema(description = "토스 결제 취소를 할 때 필요한 정보")
 public class CancelDto {
-  private Long paymentId;
-  private String CancelReason;
+  @Schema(description = "취소 금액")
+  private Long balance;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSSSSS", timezone = "Asia/Seoul")
+  @Schema(description = "토스 결제 승인 날짜와 시간")
+  private LocalDateTime createTime;
+  @Schema(description = "토스 결제 취소 이유")
+  private String cancelReason;
 }
